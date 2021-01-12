76ca3239d1ed62d5e57e0b6c455e3de9f6672149
rha <rha@dhigroup.com>
2021-01-12 14:09:58 +0100

    Hampel filter draft

---
+++ anomalydetection/hampel.py
@@ -0,0 +1,54 @@
+import numpy as np
+import pandas as pd
+
+
+def median_absolute_deviation(x):
+    """
+    Returns the median absolute deviation from the window's median
+    :param x: Values in the window
+    :return: MAD
+    """
+    return np.median(np.abs(x - np.median(x)))
+
+
+def hampel(ts, window_size=5, n=3):
+
+    """
+    Median absolute deviation (MAD) outlier in Time Series
+    :param ts: a pandas Series object representing the timeseries
+    :param window_size: total window size will be computed as 2*window_size + 1
+    :param n: threshold, default is 3 (Pearson's rule)
+    :return: Returns the corrected timeserie
+    """
+
+    if type(ts) != pd.Series:
+        raise ValueError("Timeserie object must be of tyme pandas.Series.")
+
+    if type(window_size) != int:
+        raise ValueError("Window size must be of type integer.")
+    else:
+        if window_size <= 0:
+            raise ValueError("Window size must be more than 0.")
+
+    if type(n) != int:
+        raise ValueError("Window size must be of type integer.")
+    else:
+        if n < 0:
+            raise ValueError("Window size must be equal or more than 0.")
+
+    # Copy the Series object. This will be the cleaned timeserie
+    ts_cleaned = ts.copy()
+
+    # Constant scale factor, which depends on the distribution
+    # In this case, we assume normal distribution
+    k = 1.4826
+
+    rolling_ts = ts_cleaned.rolling(window_size*2, center=True)
+    rolling_median = rolling_ts.median().fillna(method='bfill').fillna(method='ffill')
+    rolling_sigma = k*(rolling_ts.apply(median_absolute_deviation).fillna(method='bfill').fillna(method='ffill'))
+
+    outlier_indices = list(
+        np.array(np.where(np.abs(ts_cleaned - rolling_median) >= (n * rolling_sigma))).flatten())
+    ts_cleaned[outlier_indices] = rolling_median[outlier_indices]
+
+    return ts_cleaned
