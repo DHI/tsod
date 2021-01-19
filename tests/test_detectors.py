b2ba3d7138e33f70bc9f9e1202a63a7baca2fd6c
rha <rha@dhigroup.com>
2021-01-19 15:04:04 +0100

    Return normal data = random walk without outliers

--- tests/test_detectors.py
+++ tests/test_detectors.py
@@ -11,9 +11,9 @@ from tests.data_generation import create_random_walk_with_outliers
 @pytest.fixture
 def data_series():
     n_steps = 100
-    time_series_with_outliers, outlier_indices = create_random_walk_with_outliers(n_steps)
+    time_series_with_outliers, outlier_indices, random_walk = create_random_walk_with_outliers(n_steps)
     time = pd.date_range(start='2020', periods=n_steps, freq='1H')
-    return pd.Series(time_series_with_outliers, index=time), outlier_indices
+    return pd.Series(time_series_with_outliers, index=time), outlier_indices, random_walk
 
 
 @pytest.fixture
@@ -85,9 +85,9 @@ def test_peak_detector(range_data_series):
 
 
 def test_hampel_detector(data_series):
-    data, expected_anomalies_indices = data_series
+    data_with_anomalies, expected_anomalies_indices, data = data_series
     detector = HampelDetector()
-    anomalies = detector.detect(data)
+    anomalies = detector.detect(data_with_anomalies)
     anomalies_indices = np.array(np.where(anomalies)).flatten()
     # Validate if the found anomalies are also in the expected anomaly set
     # NB Not necessarily all of them
