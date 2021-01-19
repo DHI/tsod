076a5c4f9ba059307205403e5c26edc1cb9a443a
rha <rha@dhigroup.com>
2021-01-19 15:31:10 +0100

    Test autoencoder (only uniform time series and no lags)

--- tests/test_detectors.py
+++ tests/test_detectors.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 from anomalydetection.custom_exceptions import NoRangeDefinedError, WrongInputDataType
 from anomalydetection.detectors import RangeDetector, DiffRangeDetector, AnomalyDetectionPipeline, PeakDetector, \
-    HampelDetector
+    HampelDetector, AutoEncoder
 from tests.data_generation import create_random_walk_with_outliers
 
 
@@ -92,3 +92,14 @@ def test_hampel_detector(data_series):
     # Validate if the found anomalies are also in the expected anomaly set
     # NB Not necessarily all of them
     assert all(i in expected_anomalies_indices for i in anomalies_indices)
+
+
+def test_autoencoder_detector(data_series):
+    data_with_anomalies, expected_anomalies_indices, normal_data = data_series
+    detector = AutoEncoder(hidden_neurons=[1, 1, 1, 1])  # TODO add lagged features to increase layer size
+    detector.fit(normal_data)
+    anomalies = detector.detect(data_with_anomalies)
+    anomalies_indices = np.array(np.where(anomalies)).flatten()
+    # Validate if the found anomalies are also in the expected anomaly set
+    # NB Not necessarily all of them
+    # assert all(i in expected_anomalies_indices for i in anomalies_indices)
