cfa4747750a775d5c2bbfebfca274a26fd404c7c
rha <rha@dhigroup.com>
2021-01-20 16:47:37 +0100

    Test numba implementation gives same output

--- tests/test_detectors.py
+++ tests/test_detectors.py
@@ -93,12 +93,7 @@ def test_hampel_detector(data_series):
     # NB Not necessarily all of them
     assert all(i in expected_anomalies_indices for i in anomalies_indices)
 
-
-def test_hampel_numba_detector(data_series):
-    data_with_anomalies, expected_anomalies_indices, _ = data_series
-    detector = HampelDetector(use_numba=True)
-    anomalies = detector.detect(data_with_anomalies)
-    anomalies_indices = np.array(np.where(anomalies)).flatten()
-    # Validate if the found anomalies are also in the expected anomaly set
-    # NB Not necessarily all of them
-    assert all(i in expected_anomalies_indices for i in anomalies_indices)
+    detector_numba = HampelDetector(use_numba=True)
+    anomalies_numba = detector_numba.detect(data_with_anomalies)
+    anomalies_indices_numba = np.array(np.where(anomalies_numba)).flatten()
+    assert all(i in anomalies_indices_numba for i in anomalies_indices)
