2d5aee6d3fea14c80c1c813ec6ad0b0ccc563229
rha <rha@dhigroup.com>
2021-03-05 09:26:58 +0100

    Move detectors with big optional dependencies to own files

---
+++ anomalydetection/autoencoders.py
@@ -0,0 +1,29 @@
+import pandas as pd
+from pyod.models.auto_encoder import AutoEncoder as AutoEncoderPyod
+
+from anomalydetection.detectors import BaseDetector
+
+
+class AutoEncoder(BaseDetector):
+    def __init__(self, **kwargs):
+        super().__init__()
+        self._model = AutoEncoderPyod(**kwargs)
+
+    def fit(self, data):
+        data = self._validate(data)
+        self._model.fit(data)
+
+        return self
+
+    def detect(self, data):
+        data = self._validate(data)
+        return self._model.predict(data)
+
+    def _validate(self, data):
+        if isinstance(data, pd.Series):
+            return data.values.reshape(-1, 1)
+        else:
+            return data
+
+    def __str__(self):
+        return f"{self.__class__.__name__}({self._model})"
--- anomalydetection/detectors.py
+++ anomalydetection/detectors.py
@@ -2,8 +2,6 @@ import pandas as pd
 import numpy as np
 
 from anomalydetection.custom_exceptions import WrongInputDataType, NoRangeDefinedError, NonUniqueTimeStamps
-from anomalydetection import hampel
-from pyod.models.auto_encoder import AutoEncoder as AutoEncoderPyod
 
 
 class BaseDetector:
@@ -183,57 +181,6 @@ class RollingStandardDeviationDetector(BaseDetector):
 
     def __str__(self):
         return f"{self.__class__.__name__}({self._window_size}, {self._threshold})"
-
-
-class HampelDetector(BaseDetector):
-    def __init__(self, window_size=5, threshold=3, use_numba=False):
-        super().__init__()
-        hampel.validate_arguments(window_size, threshold)
-        self._threshold = threshold
-        self._window_size = window_size
-        self._use_numba = use_numba
-
-    def detect(self, data):
-        super().validate(data)
-
-        if self._use_numba:
-            anomalies, indices, _ = hampel.detect_using_numba(
-                data.values, self._window_size, self._threshold
-            )
-        else:
-            anomalies, indices, _ = hampel.detect(
-                data, self._window_size, self._threshold
-            )
-
-        return anomalies
-
-    def __str__(self):
-        return f"{self.__class__.__name__}({self._window_size}, {self._threshold})"
-
-
-class AutoEncoder(BaseDetector):
-    def __init__(self, **kwargs):
-        super().__init__()
-        self._model = AutoEncoderPyod(**kwargs)
-
-    def fit(self, data):
-        data = self._validate(data)
-        self._model.fit(data)
-
-        return self
-
-    def detect(self, data):
-        data = self._validate(data)
-        return self._model.predict(data)
-
-    def _validate(self, data):
-        if isinstance(data, pd.Series):
-            return data.values.reshape(-1, 1)
-        else:
-            return data
-
-    def __str__(self):
-        return f"{self.__class__.__name__}({self._model})"
 
 
 class ConstantValueDetector(BaseDetector):
