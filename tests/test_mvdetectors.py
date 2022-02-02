d42bef9dcfffa0f3ede7e905ab9fe52bf00dc605
laurafroelich <lafr@dhigroup.com>
2022-02-02 14:06:45 +0100

    Add test for fitting and make tests for different initializing values more compact

--- tests/test_mvdetectors.py
+++ tests/test_mvdetectors.py
@@ -18,42 +18,34 @@ def range_data():
     return normal_data, abnormal_data
 
 
-def test_mv_min_max_range_detector(range_data):
+@pytest.mark.parametrize("detector, expected_anomalies_list", [
+    (MVRangeDetector(min_value=0.0, max_value=1.0),
+     [False, False, True, True, False, False, False, True, False, False, False, False, True, False, False]),
+    (MVRangeDetector(max_value=1.0),
+     [False, False, True, True, False, False, False, True, False, False, False, False, False, False, False]),
+    (MVRangeDetector(min_value=0.0),
+     [False, False, True, False, False, False, False, False, False, False, False, False, True, False, False]),
+])
+def test_range_detector_detection(range_data, detector, expected_anomalies_list):
     normal_data, abnormal_data = range_data
     n_obs = normal_data.shape[1]
-    detector = MVRangeDetector(min_value=0.0, max_value=1.0)
+    detected_anomalies = detector.detect(abnormal_data)
+    expected_anomalies = pd.Series(expected_anomalies_list, index=pd.Int64Index(np.arange(n_obs), dtype='int64'))
+    pd.testing.assert_series_equal(expected_anomalies, detected_anomalies)
+
+    detected_anomalies = detector.detect(normal_data)
+    assert not any(detected_anomalies)
+
+
+def test_range_detector_fitting(range_data):
+    normal_data, abnormal_data = range_data
+    detector = MVRangeDetector()
+    detector.fit(normal_data)
+    n_obs = normal_data.shape[1]
+    detected_anomalies = detector.detect(abnormal_data)
     expected_anomalies = pd.Series(
         [False, False, True, True, False, False, False, True, False, False, False, False, True, False, False],
         index=pd.Int64Index(np.arange(n_obs), dtype='int64'))
-    detected_anomalies = detector.detect(abnormal_data)
-    pd.testing.assert_series_equal(expected_anomalies, detected_anomalies)
-
-    detected_anomalies = detector.detect(normal_data)
-    assert not any(detected_anomalies)
-
-
-def test_mv_max_range_detector(range_data):
-    normal_data, abnormal_data = range_data
-    n_obs = normal_data.shape[1]
-    detector = MVRangeDetector(max_value=1.0)
-    expected_anomalies = pd.Series(
-        [False, False, True, True, False, False, False, True, False, False, False, False, False, False, False],
-        index=pd.Int64Index(np.arange(n_obs), dtype='int64'))
-    detected_anomalies = detector.detect(abnormal_data)
-    pd.testing.assert_series_equal(expected_anomalies, detected_anomalies)
-
-    detected_anomalies = detector.detect(normal_data)
-    assert not any(detected_anomalies)
-
-
-def test_mv_min_range_detector(range_data):
-    normal_data, abnormal_data = range_data
-    n_obs = normal_data.shape[1]
-    detector = MVRangeDetector(min_value=0.0)
-    expected_anomalies = pd.Series(
-        [False, False, True, False, False, False, False, False, False, False, False, False, True, False, False],
-        index=pd.Int64Index(np.arange(n_obs), dtype='int64'))
-    detected_anomalies = detector.detect(abnormal_data)
     pd.testing.assert_series_equal(expected_anomalies, detected_anomalies)
 
     detected_anomalies = detector.detect(normal_data)
