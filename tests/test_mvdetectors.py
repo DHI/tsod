2bf4fb1bca9288c3242eb6bdd91c4f14dc7402fb
laurafroelich <lafr@dhigroup.com>
2022-02-04 11:19:10 +0100

    Add tests for time-series specific ranges

--- tests/test_mvdetectors.py
+++ tests/test_mvdetectors.py
@@ -10,9 +10,26 @@ def range_data():
     n_obs = 15
     normal_data = pd.DataFrame(np.random.uniform(size=[3, n_obs]))
     normal_data.iloc[2, [2, 8]] = np.nan
-    normal_data.iloc[2, [0]] = 1
-    normal_data.iloc[2, [1]] = 0
+    normal_data.iloc[:, 13] = 1
+    normal_data.iloc[:, 14] = 0
     abnormal_data = pd.DataFrame(np.random.uniform(size=[3, n_obs]))
+    abnormal_data.iloc[0, [2, 3, 7]] = 5
+    abnormal_data.iloc[1, [2, 12]] = -2
+    abnormal_data.iloc[0, [8]] = np.nan
+    abnormal_data.iloc[2, [8, 9]] = np.nan
+    return normal_data, abnormal_data
+
+
+@pytest.fixture
+def range_data_time_series_specific_ranges():
+    n_obs = 15
+    ts_mins = [-1, -0.5, 0]
+    ts_maxs = [2, 3, 4]
+    normal_data = pd.DataFrame(np.random.uniform(low=ts_mins, high=ts_maxs, size=(n_obs, len(ts_mins))).T)
+    normal_data.iloc[2, [2, 8]] = np.nan
+    normal_data.iloc[:, 13] = ts_mins
+    normal_data.iloc[:, 14] = ts_maxs
+    abnormal_data = pd.DataFrame(np.random.uniform(low=ts_mins, high=ts_maxs, size=(n_obs, len(ts_mins))).T)
     abnormal_data.iloc[0, [2, 3, 7]] = 5
     abnormal_data.iloc[1, [2, 12]] = -2
     abnormal_data.iloc[0, [8]] = np.nan
@@ -22,33 +39,104 @@ def range_data():
 
 @pytest.mark.parametrize("detector, expected_anomalies_list", [
     (MVRangeDetector(min_value=0.0, max_value=1.0),
-     [False, False, True, True, False, False, False, True, False, False, False, False, True, False, False]),
+     [[False, False, True, True, False, False, False, True, False, False, False, False, False, False, False],
+     [False, False, True, False, False, False, False, False, False, False, False, False, True, False, False],
+     [False, False, False, False, False, False, False, False, False, False, False, False, False, False, False]]),
     (MVRangeDetector(max_value=1.0),
-     [False, False, True, True, False, False, False, True, False, False, False, False, False, False, False]),
+     [[False, False, True, True, False, False, False, True, False, False, False, False, False, False, False],
+     [False, False, False, False, False, False, False, False, False, False, False, False, False, False, False],
+     [False, False, False, False, False, False, False, False, False, False, False, False, False, False, False]]),
     (MVRangeDetector(min_value=0.0),
-     [False, False, True, False, False, False, False, False, False, False, False, False, True, False, False]),
+     [[False, False, False, False, False, False, False, False, False, False, False, False, False, False, False],
+     [False, False, True, False, False, False, False, False, False, False, False, False, True, False, False],
+     [False, False, False, False, False, False, False, False, False, False, False, False, False, False, False]])
 ])
-def test_range_detector_detection(range_data, detector, expected_anomalies_list):
+def test_single_range_detector_detection(range_data, detector, expected_anomalies_list):
     normal_data, abnormal_data = range_data
-    n_obs = normal_data.shape[1]
     detected_anomalies = detector.detect(abnormal_data)
-    expected_anomalies = pd.Series(expected_anomalies_list, index=pd.Int64Index(np.arange(n_obs), dtype='int64'))
-    pd.testing.assert_series_equal(expected_anomalies, detected_anomalies)
+    expected_anomalies = pd.DataFrame(expected_anomalies_list, columns=abnormal_data.columns, index=abnormal_data.index)
+    pd.testing.assert_frame_equal(expected_anomalies, detected_anomalies)
 
     detected_anomalies = detector.detect(normal_data)
-    assert not any(detected_anomalies)
+    assert not detected_anomalies.to_numpy().any()
 
 
-def test_range_detector_fitting(range_data):
+def test_single_range_detector_fitting(range_data):
     normal_data, abnormal_data = range_data
     detector = MVRangeDetector()
     detector.fit(normal_data)
-    n_obs = normal_data.shape[1]
     detected_anomalies = detector.detect(abnormal_data)
-    expected_anomalies = pd.Series(
-        [False, False, True, True, False, False, False, True, False, False, False, False, True, False, False],
-        index=pd.Int64Index(np.arange(n_obs), dtype='int64'))
-    pd.testing.assert_series_equal(expected_anomalies, detected_anomalies)
+    expected_anomalies = pd.DataFrame(
+        [[False, False, True, True, False, False, False, True, False, False, False, False, False, False, False],
+         [False, False, True, False, False, False, False, False, False, False, False, False, True, False, False],
+         [False, False, False, False, False, False, False, False, False, False, False, False, False, False, False]],
+        columns=abnormal_data.columns, index=abnormal_data.index)
+    pd.testing.assert_frame_equal(expected_anomalies, detected_anomalies)
 
     detected_anomalies = detector.detect(normal_data)
-    assert not any(detected_anomalies)
+    assert not detected_anomalies.to_numpy().any()
+
+
+@pytest.mark.parametrize("detector, expected_anomalies_list", [
+    (MVRangeDetector(min_value=[0.0, 0.0, 0.0], max_value=1.0),
+     [[False, False, True, True, False, False, False, True, False, False, False, False, False, False, False],
+     [False, False, True, False, False, False, False, False, False, False, False, False, True, False, False],
+     [False, False, False, False, False, False, False, False, False, False, False, False, False, False, False]]),
+    (MVRangeDetector(min_value=0.0, max_value=[1.0, 1.0, 1.0]),
+     [[False, False, True, True, False, False, False, True, False, False, False, False, False, False, False],
+     [False, False, True, False, False, False, False, False, False, False, False, False, True, False, False],
+     [False, False, False, False, False, False, False, False, False, False, False, False, False, False, False]]),
+    (MVRangeDetector(min_value=[0.0, 0.0, 0.0], max_value=[1.0, 1.0, 1.0]),
+     [[False, False, True, True, False, False, False, True, False, False, False, False, False, False, False],
+     [False, False, True, False, False, False, False, False, False, False, False, False, True, False, False],
+     [False, False, False, False, False, False, False, False, False, False, False, False, False, False, False]])
+])
+def test_multi_range_detector_detection(range_data, detector, expected_anomalies_list):
+    normal_data, abnormal_data = range_data
+    detected_anomalies = detector.detect(abnormal_data)
+    expected_anomalies = pd.DataFrame(expected_anomalies_list, columns=abnormal_data.columns, index=abnormal_data.index)
+    pd.testing.assert_frame_equal(expected_anomalies, detected_anomalies)
+
+    detected_anomalies = detector.detect(normal_data)
+
+    assert not detected_anomalies.to_numpy().any()
+
+
+@pytest.mark.parametrize("detector, expected_anomalies_list", [
+    (MVRangeDetector(min_value=[-1, -0.5, 0], max_value=[2, 3, 4]),
+     [[False, False, True, True, False, False, False, True, False, False, False, False, False, False, False],
+     [False, False, True, False, False, False, False, False, False, False, False, False, True, False, False],
+     [False, False, False, False, False, False, False, False, False, False, False, False, False, False, False]]),
+    (MVRangeDetector(max_value=[2, 3, 4]),
+     [[False, False, True, True, False, False, False, True, False, False, False, False, False, False, False],
+     [False, False, False, False, False, False, False, False, False, False, False, False, False, False, False],
+     [False, False, False, False, False, False, False, False, False, False, False, False, False, False, False]]),
+    (MVRangeDetector(min_value=[-1, -0.5, 0]),
+     [[False, False, False, False, False, False, False, False, False, False, False, False, False, False, False],
+     [False, False, True, False, False, False, False, False, False, False, False, False, True, False, False],
+     [False, False, False, False, False, False, False, False, False, False, False, False, False, False, False]])
+])
+def test_multiple_ranges_detector_detection(range_data_time_series_specific_ranges, detector, expected_anomalies_list):
+    normal_data, abnormal_data = range_data_time_series_specific_ranges
+    detected_anomalies = detector.detect(abnormal_data)
+    expected_anomalies = pd.DataFrame(expected_anomalies_list, columns=abnormal_data.columns, index=abnormal_data.index)
+    pd.testing.assert_frame_equal(expected_anomalies, detected_anomalies)
+
+    detected_anomalies = detector.detect(normal_data)
+    assert not detected_anomalies.to_numpy().any()
+
+
+def test_multiple_ranges_detector_fitting(range_data_time_series_specific_ranges):
+    normal_data, abnormal_data = range_data_time_series_specific_ranges
+    detector = MVRangeDetector()
+    detector.fit(normal_data)
+    detected_anomalies = detector.detect(abnormal_data)
+    expected_anomalies = pd.DataFrame(
+        [[False, False, True, True, False, False, False, True, False, False, False, False, False, False, False],
+         [False, False, True, False, False, False, False, False, False, False, False, False, True, False, False],
+         [False, False, False, False, False, False, False, False, False, False, False, False, False, False, False]],
+        columns=abnormal_data.columns, index=abnormal_data.index)
+    pd.testing.assert_frame_equal(expected_anomalies, detected_anomalies)
+
+    detected_anomalies = detector.detect(normal_data)
+    assert not detected_anomalies.to_numpy().any()
