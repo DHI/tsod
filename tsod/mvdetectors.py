facaf0bd22111764122bb77f04d2ce8cc5ed2023
laurafroelich <lafr@dhigroup.com>
2022-02-04 11:17:46 +0100

    Add support for time series specific ranges

--- tsod/mvdetectors.py
+++ tsod/mvdetectors.py
@@ -1,22 +1,35 @@
 import pandas as pd
 import numpy as np
-from typing import Union
+import typing
 
 from .base import Detector
+from .custom_exceptions import NoRangeDefinedError, WrongInputSize
+
+
+def make_vector_broadcastable(function_input, n_data_rows):
+    if function_input is not None:
+        if len(function_input.shape) > 0:
+            if len(function_input) != n_data_rows:
+                raise WrongInputSize(
+                    "The number of rows in the input data must match the number of "
+                    "values specified for min and max if more than one value is given for min/max.")
+    min_comparison = function_input
+    if len(function_input.shape) == 1:
+        min_comparison = function_input[..., np.newaxis]
+    return min_comparison
 
 
 class MVRangeDetector(Detector):
     """
     Detect values outside range.
 
-    If one or more time series is out of range, is is detected as an anomaly. Note that this implies that the same range
-    is used for all time series.
+    NaN values are not marked as anomalies.
 
     Parameters
     ----------
-    min_value : float
+    min_value : float, List, np.array
         Minimum value threshold.
-    max_value : float
+    max_value : float, List, np.array
         Maximum value threshold.
     quantile_prob_cut_offs : list[2]
                 Default quantiles [0, 1]. Same as min and max value.
@@ -44,11 +57,17 @@ class MVRangeDetector(Detector):
     def __init__(self, min_value=-np.inf, max_value=np.inf, quantile_prob_cut_offs=None):
         super().__init__()
 
+        min_value = np.array(min_value)
+        assert len(min_value.shape) <= 1
+
+        max_value = np.array(max_value)
+        assert len(max_value.shape) <= 1
+
+        assert np.array([min_value <= max_value]).all()
+
         self._min = min_value
 
         self._max = max_value
-
-        assert self._min <= self._max
 
         if quantile_prob_cut_offs is None:
             self.quantile_prob_cut_offs = [0.0, 1.0]
@@ -62,30 +81,38 @@ class MVRangeDetector(Detector):
 
         Parameters
         ----------
-        data :  pd.Series
-                Normal time series data.
+        data :  pd.DataFrame
+                Time series data with time over columns.
         """
         super().validate(data)
 
-        quantiles = np.nanquantile(data, self.quantile_prob_cut_offs)
+        quantiles = np.nanquantile(data, self.quantile_prob_cut_offs, axis=1)
         self._min = quantiles[0]
         self._max = quantiles[1]
 
         return self
 
-    def _detect(self, data: Union[pd.Series, pd.DataFrame]) -> pd.Series:
+    def _detect(self, data: typing.Union[pd.Series, pd.DataFrame]) -> pd.DataFrame:
         """Detect anomalies outside range"""
 
-        minimum_values = data.min(axis=0)
-        maximum_values = data.max(axis=0)
+        if (self._min is None) and (self._max is None):
+            raise NoRangeDefinedError("Both min and max are None. At least one of them must be set.")
+
+        if len(data.shape) == 1:
+            n_data_rows = 1
+        else:
+            n_data_rows = data.shape[0]
+
+        min_comparison = make_vector_broadcastable(self._min, n_data_rows)
+        max_comparison = make_vector_broadcastable(self._max, n_data_rows)
 
         if self._max is None:
-            return minimum_values < self._min
+            return data < min_comparison
 
         if self._min is None:
-            return maximum_values > self._max
+            return data > max_comparison
 
-        return (minimum_values < self._min) | (maximum_values > self._max)
+        return (data < min_comparison) | (data > max_comparison)
 
     def __str__(self):
 
