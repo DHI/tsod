d107202a3e5d5b873a173b847fe7e98e2adb870b
Henrik Andersson <jan@dhigroup.com>
2023-09-01 14:16:08 +0200

    Ruff config

--- tsod/__init__.py
+++ tsod/__init__.py
@@ -13,3 +13,16 @@ from .mvdetectors import MVRangeDetector, MVCorrelationDetector
 from .base import load
 
 __version__ = "0.1.4"
+
+__all__ = [
+    "RangeDetector",
+    "DiffDetector",
+    "ConstantGradientDetector",
+    "GradientDetector",
+    "ConstantValueDetector",
+    "CombinedDetector",
+    "RollingStandardDeviationDetector",
+    "MVRangeDetector",
+    "MVCorrelationDetector",
+    "load",
+]
